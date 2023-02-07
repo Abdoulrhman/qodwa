22cef801f878cc4e568b2d604538e0973fb7581d
Abdoulrhman <Abdoulrahman@holo.ae>
2023-02-07 21:36:31 +0200

    Handle Nav in MObile

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,5 +1,5 @@
 // create header component with logo and navigation links
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Logo from '../../assets/images/logo.png';
 import './styles.scss';
@@ -7,31 +7,38 @@ import './styles.scss';
 const Header = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [isNavExpanded, setIsNavExpanded] = useState(true);
 
   const handleNavigate = path => {
     navigate(path);
   };
 
+  const handleToggleNav = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
+
   return (
-    <div className="header">
-      <div className="header__logo">
-        <div className="header__logo-text">
-          <img src={Logo} alt="Logo" />
-        </div>
-      </div>
-      <nav className="header__nav">
-        <ul className="header__nav-list">
-          <li className="header__nav-item" onClick={() => handleNavigate('/')} aria-hidden>
-            Home
+    <nav className="navigation">
+      <img src={Logo} alt="Logo" className="brand-name" />
+      <button className="hamburger" type="button" onClick={handleToggleNav}>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M21 18H3V16H21V18ZM21 13H3V11H21V13ZM21 8H3V6H21V8Z" fill="#FFF" />
+        </svg>
+      </button>
+      <div className={isNavExpanded ? 'navigation-menu expanded' : 'navigation-menu'}>
+        <ul>
+          <li>
+            <a href="/home">About Us</a>
           </li>
-          <li className="header__nav-item" onClick={() => handleNavigate('/about-us')} aria-hidden>
-            About Us
+          <li>
+            <a href="/about">About</a>
           </li>
-          <li className="header__nav-item">Pricing</li>
-          <li className="header__nav-item">Contact Us</li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
         </ul>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
